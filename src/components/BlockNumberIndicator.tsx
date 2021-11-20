931babdbf1986f058c43a1483c50f603ffe84f87
Jay Welsh <thealchemicalopus@gmail.com>
2021-11-21 00:50:47 +0200

    feat(BlockNumberIndicator): improves block number indicator

--- src/components/BlockNumberIndicator.tsx
+++ src/components/BlockNumberIndicator.tsx
@@ -1,7 +1,6 @@
-import React, {useEffect, useState} from 'react';
-
+import React, {useEffect, useState, useRef} from 'react';
 import styled, { keyframes } from 'styled-components'
-
+import Typography from '@mui/material/Typography';
 import { useBlockNumber, useEthers } from '@usedapp/core'
 
 import { getEtherscanLink } from '../utils';
@@ -35,7 +34,11 @@ const Spinner = styled.div`
   top: -3px;
 `
 
-const StyledPollingDot = styled.div`
+interface IStyledPollingDot {
+  blockSyncStatus: string;
+}
+
+const StyledPollingDot = styled.div<IStyledPollingDot>`
   width: 8px;
   height: 8px;
   min-height: 8px;
@@ -43,7 +46,19 @@ const StyledPollingDot = styled.div`
   margin-right: 5px;
   border-radius: 50%;
   position: relative;
-  background-color: lime;
+  background-color: ${
+    (props) => {
+      if(props.blockSyncStatus === 'fresh') {
+        return 'lime';
+      } else if (props.blockSyncStatus === 'delayed') {
+        return 'orange';
+      } else if (props.blockSyncStatus === 'disconnected') {
+        return 'red';
+      } else if (props.blockSyncStatus === 'connecting') {
+        return 'grey';
+      }
+    }
+  };
 `
 
 const BlockNumberContainer = styled.div`
@@ -67,22 +82,43 @@ const BlockNumberIndicator = () => {
     const blockNumber = useBlockNumber()
     const { chainId } = useEthers()
     const [showSpinner, setShowSpinner] = useState(false)
+    const [blockSyncStatus, setBlockSyncStatus] = useState('connecting');
+
+    let timer1 = useRef(setTimeout(() => {}, 1))
+    let timer2 = useRef(setTimeout(() => {}, 1))
+    let timer3 = useRef(setTimeout(() => {}, 1))
+
     useEffect(() => {
+        if(timer1.current) {
+          clearTimeout(timer1.current)
+        }
+        timer1.current = setTimeout(() => setShowSpinner(false), 1000)
+        if(timer2.current) {
+          clearTimeout(timer2.current)
+        }
+        timer2.current = setTimeout(() => setBlockSyncStatus('delayed'), 45000)
+        if(timer3.current) {
+          clearTimeout(timer3.current)
+        }
+        timer3.current = setTimeout(() => setBlockSyncStatus('disconnected'), 90000)
+
         setShowSpinner(true);
-        const timer1 = setTimeout(() => setShowSpinner(false), 1000)
+        setBlockSyncStatus('fresh');
 
         // this will clear Timeout when component unmount like in willComponentUnmount
         return () => {
             setShowSpinner(false)
-            clearTimeout(timer1)
+            clearTimeout(timer1.current)
+            clearTimeout(timer2.current)
+            clearTimeout(timer3.current)
         }
     }, [blockNumber])
-    return (
+    return (blockNumber ?
         <ExternalLink href={chainId && blockNumber ? getEtherscanLink(chainId, blockNumber.toString(), 'block') : ''}>
             <BlockNumberContainer>
-                <StyledPollingDot>{showSpinner && <Spinner/>}</StyledPollingDot><span style={{opacity: !showSpinner ? '0.4' : '0.6'}}>{blockNumber}</span>
+                <StyledPollingDot blockSyncStatus={blockSyncStatus}>{showSpinner && <Spinner/>}</StyledPollingDot><span style={{opacity: !showSpinner ? '0.6' : '0.8'}}><Typography variant="overline" style={{fontFamily: 'monospace', lineHeight: 0}}>{blockNumber}</Typography></span>
             </BlockNumberContainer>
-        </ExternalLink>
+        </ExternalLink> : null
     )
 }
 
